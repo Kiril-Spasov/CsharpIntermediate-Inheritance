25507f297020d27dc424d555499fc9d031cc5956
spaso <spaso@DESKTOP-FEK00DU>
2022-06-02 15:25:08 +0300

    Add project files.

---
+++ exercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace exercise
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+        }
+    }
+}
---
+++ exercise/Stack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace exercise
+{
+    internal class Stack
+    {
+        private readonly List<object> _list;
+        private int _topItemIndex = -1;
+        private object _popItem;
+
+        public Stack()
+        {
+           _list = new List<object>();
+           
+        }
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("Null is not valid");
+
+            _list.Add(obj);
+            _topItemIndex++;
+        }
+
+        public object Pop()
+        {
+            if (_list.Count == 0)
+                throw new InvalidOperationException("Stack is empty");
+
+            _popItem = _list[_topItemIndex];
+            _list.RemoveAt(_topItemIndex);
+            _topItemIndex--;
+            return _popItem;
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+    }
+}
